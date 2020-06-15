d7f34a3d6e31e843e60870844fc2926cc54cff67
Chanjong Baek <readandaction@gmail.com>
2020-06-15 12:21:27 +0900

    3.6 recap

--- users/models.py
+++ users/models.py
@@ -20,19 +20,15 @@ class User(AbstractUser):
     LANGUAGE_KOR = "kr"
     LANGUAGE_EN = "en"
 
-    LANGUAGE_CHOICES = ((LANGUAGE_KOR, "Kr"), (LANGUAGE_EN, "En"))
+    LANGUAGE_CHOICES = ((LANGUAGE_KOR, "Korea"), (LANGUAGE_EN, "English"))
 
     CURRENCY_KOREAN = "kwr"
     CURRENCY_USD = "usd"
     CURRENCY_CHOICES = ((CURRENCY_KOREAN, "KWR"), (CURRENCY_USD, "USD"))
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, null=True, max_length=10)
-    bio = models.TextField(default="")
-    birthdate = models.DateField(null=True)
-    langauge = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
-    )
-    currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
-    )
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    langauge = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
