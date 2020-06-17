70508e42ba68fa8cbc8e1272dea3179a211faa22
Chanjong Baek <readandaction@gmail.com>
2020-06-17 15:21:48 +0900

    5.2 Add List Model

---
+++ lists/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from core import models as core_models
+
+
+class List(core_models.TimeStampedModel):
+
+    """ List Model Definition"""
+
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    rooms = models.ManyToManyField("rooms.Room")
+
+    def __str__(self):
+        return self.name
