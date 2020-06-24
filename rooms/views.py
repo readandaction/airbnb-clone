1c57be6088f69196da944860b041510800fbcf87
Chanjong Baek <readandaction@gmail.com>
2020-06-24 22:40:33 +0900

    11.1 How to use request.GET

--- rooms/views.py
+++ rooms/views.py
@@ -6,7 +6,10 @@ from . import models
 
 
 def all_rooms(request):
-    now = datetime.now()
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"now": now, "rooms": all_rooms})
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
 
