ef68b11cdb850247c11bf5ab33cccc9e343d69a2
Chanjong Baek <readandaction@gmail.com>
2020-06-24 11:23:49 +0900

    10.3 How to use template extends, logic

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
 
 def all_rooms(request):
     now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"now": now, "rooms": all_rooms})
 
