9ac19b50e98fe0a6a99feef143622ea9a21ef784
Chanjong Baek <readandaction@gmail.com>
2020-06-24 10:30:05 +0900

    10.1 Learn http and render

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def all_rooms(request):
-    pass
+    # now = datetime.now()
+    # return HttpResponse(content=f"<h1>{now}</h1>")
+    return render(request, "all_rooms")
+
