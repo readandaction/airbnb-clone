b55012771ef8861cd32f84e833e5777257c1f63a
Chanjong Baek <readandaction@gmail.com>
2020-06-24 10:55:07 +0900

    10.2 How to use Templates

--- rooms/views.py
+++ rooms/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 
 def all_rooms(request):
-    # now = datetime.now()
-    # return HttpResponse(content=f"<h1>{now}</h1>")
-    return render(request, "all_rooms")
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
 
