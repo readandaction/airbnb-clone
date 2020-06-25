b2abbea92122fcf25c554542b61911b4faf2b3aa
Chanjong Baek <readandaction@gmail.com>
2020-06-25 12:13:29 +0900

    11.7 How to use class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,14 @@
-from math import ceil
-from datetime import datetime
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", context={"page": rooms},)
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
 
+    """ Home View Defintion """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "price"
