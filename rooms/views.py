91b2e556bc1c73fbdd734064befd458b4c2df463
Chanjong Baek <readandaction@gmail.com>
2020-06-25 11:42:20 +0900

    11.6 try except EmptyPage

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 from math import ceil
 from datetime import datetime
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # Create your views here.
@@ -11,6 +11,9 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"page": rooms},)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={"page": rooms},)
+    except EmptyPage:
+        return redirect("/")
 
