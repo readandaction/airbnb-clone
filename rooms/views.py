0836c32532cb81df9b926cdab5a8f422906e2c56
Chanjong Baek <readandaction@gmail.com>
2020-06-25 13:01:44 +0900

    11.8 context in CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
 # Create your views here.
@@ -12,3 +13,10 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "price"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
