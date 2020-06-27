5bd765af9378713965967ea9f94b1d0af49f221e
Chanjong Baek <readandaction@gmail.com>
2020-06-27 10:22:14 +0900

    13.1 Make Search_bar block

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.urls import reverse
+from django_countries import countries
 from . import models
 
 # Create your views here.
@@ -35,7 +36,12 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
 
