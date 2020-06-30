b2041c3c38a1f405b5515fe5bbf8dbc3243d2709
Chanjong Baek <readandaction@gmail.com>
2020-06-30 15:16:08 +0900

    14.4 Add authenticate, login, logout

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from . import forms
 
@@ -7,14 +8,26 @@ class LoginView(View):
     """ LoginView Definition """
 
     def get(self, request):
-        form = forms.LoginForm(initial={"email": "hswanson@smith.com"})
+        form = forms.LoginForm(initial={"email": "read@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    """ logout definition"""
+    logout(request)
+    return redirect(reverse("users:login"))
 
 
 # def login(request):
