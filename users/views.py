f2ec53bc9d4f002442b5ca21ab9d9d170eb60cff
Chanjong Baek <readandaction@gmail.com>
2020-06-30 10:35:52 +0900

    14.1 Create LoginForim

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from . import forms
 
 
 class LoginView(View):
     """ LoginView Definition """
 
     def get(self, request):
-        return render(request, "users/login.html")
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        print(form)
 
 
 # def login(request):
