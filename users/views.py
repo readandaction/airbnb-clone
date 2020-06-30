b13640fcb58f8a23b57430a50e51d7bdc0b4d6db
Chanjong Baek <readandaction@gmail.com>
2020-06-30 10:12:24 +0900

    Create users login url

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    """ LoginView Definition """
+
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
+
+
+# def login(request):
+#     if request.GET.method == "get":
+#     else request.GET.method == "post":
