187ff8e5ad4bf74fd991a6247a66c645d37ddacb
Chanjong Baek <readandaction@gmail.com>
2020-07-02 11:43:16 +0900

    How to send html_message

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ import uuid
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractUser
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.db import models
 
 # Create your models here
@@ -46,10 +48,14 @@ class User(AbstractUser):
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
             send_mail(
                 "Verify Airbnb Account",
-                f"Verify account this is your secret : {secret}",
+                strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
+                html_message=html_message,
                 fail_silently=False,
             )
