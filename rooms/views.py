a427d0728c6b7d5d475e6eea2efc89b05e4317c7
Chanjong Baek <readandaction@gmail.com>
2020-07-07 09:45:05 +0900

    20.7 Update room_list

--- rooms/views.py
+++ rooms/views.py
@@ -14,7 +14,7 @@ class HomeView(ListView):
     """ Home View Defintion """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     ordering = "price"
     context_object_name = "rooms"
