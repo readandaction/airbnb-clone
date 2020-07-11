8f02ba2a1c0b58dc013bc00c3c14eece55effe84
Chanjong Baek <readandaction@gmail.com>
2020-07-11 17:18:24 +0900

    Add usersMixins

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -24,9 +23,16 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
-            return redirect(reverse("core:home"))
+        return super().form_valid(form)
 
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 
 # class LoginView(View):
@@ -163,12 +169,12 @@ def github_callback(request):
         return redirect(reverse("users:login"))
 
 
-class UserProfileView(DetailView):
+class UserProfileView(mixins.LogInRequiredView, DetailView):
     model = models.User
     context_object_name = "user_obj"
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LogInRequiredView, SuccessMessageMixin, UpdateView):
     model = models.User
     fields = (
         "first_name",
@@ -192,7 +198,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
         return form
 
 
-class UpdatePasswordView(PasswordChangeView):
+class UpdatePasswordView(
+    mixins.LogInRequiredView,
+    mixins.EmailLoginOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView,
+):
 
     template_name = "users/update-password.html"
 
